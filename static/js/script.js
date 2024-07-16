e54cb63a4a0361942aef717653cfaddf843f37a7
Muhammad Tahir <155483619+realtaahir@users.noreply.github.com>
2024-07-16 07:17:22 +0000

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+// Define the function to display a message
+function displayMessage() {
+    alert('Button n1 was clicked!');
+}
+
+// Attach the function to the button's click event
+document.getElementById('n1').addEventListener('click', displayMessage);
